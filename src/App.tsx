daca30fa2bf28ac16741a27e85f9d9b662e94463
ldru <ldru@163.com>
2021-04-04 12:17:32 +0800

    ESlint has some problem ,give up

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+//import logo from './logo.svg';
+import './App.css'
+import Web3Status from './component/Web3Status'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <body>
+        <Web3Status />
+      </body>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
