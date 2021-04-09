521acddd651b365dfd0ab0d73274893173cb4219
ldru <ldru@163.com>
2021-04-10 00:13:28 +0800

    Theme OK

--- src/components/PageHeader/index.tsx
+++ src/components/PageHeader/index.tsx
@@ -4,12 +4,12 @@ import { RowBetween } from '../Row'
 import { TYPE } from '../../theme'
 
 const StyledPageHeader = styled.div`
-  padding: 0rem 1rem 0rem 1rem;
+  padding: 8px 1rem 10px 1rem;
   margin-bottom: 6px;
   width: 100%;
-  height: 30px;
+  height: 50px;
   max-width: 420px;
-  font-size:  20px;
+  font-size:  24px;
   border-bottom: 1px solid rgba(0, 0, 0, 0.3);
   color: ${({ theme }) => theme.text2};
 `
